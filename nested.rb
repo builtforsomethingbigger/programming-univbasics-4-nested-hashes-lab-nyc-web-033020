092f9a30ee8817a36e2783783623c4c300f05bcf
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-21 03:21:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,8 +115,8 @@ def changing_alan
       }
     }
 
-programmer_hash[:alan_kay][:known_for] = "GUI"
-programmer_hash[:alan_kay][:known_for]
+programmer_hash[:alan_kay][0] = "GUI"
+programmer_hash[:alan_kay][0]
 end
 
 def adding_to_dennis
